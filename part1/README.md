dec1afe0262c8730d996759a1e669922326b0415
yeseoz <w12345678992@gmail.com>
2023-03-09 10:35:28 +0900

    네이버 영화 검색 앱 완료!

--- part1/README.md
+++ part1/README.md
@@ -5,16 +5,24 @@
 - PyQT 복습
 - QtDesigner 학습
 - Naver API 뉴스검색 앱 만들기
-    - Python 3.11.2
-    - PyQt5
-    - Qt Designer
+    - Python 3.11.2  
     - Naver OpenAPI 
+    - PyQt5 / Qt Designer
+    
 
 <!-- 사이즈 조정 불가능!
 ![네이버 뉴스 앱](https://raw.githubusercontent.com/yeseoz/miniprojects/main/imges/navernews.png)
 -->
 
 <img src="https://raw.githubusercontent.com/yeseoz/miniprojects/main/imges/navernews.png" width="650" />
+
+- Naver API 영화검색 앱 만들기
+    - Python 3.11.2
+    - Naver OpenAPI 
+    - PyQt5 / Qt Designer
+    - Urllib.request 이미지 다운로드 사용 *
+
+<img src="https://raw.githubusercontent.com/yeseoz/miniprojects/main/imges/navermovie.png" width="650">
 
 ### DB 연동 GUI 프로그램 개발 학습
 
--- part1/studyPyQt/mp04_naverMovieApp.py
+++ part1/studyPyQt/mp04_naverMovieApp.py
@@ -12,7 +12,7 @@ class qtApp(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('./studyPyQt/naverApiMovie.ui',self)
-        self.setWindowIcon(QIcon('./studyPyQt/newsPaper.png'))
+        self.setWindowIcon(QIcon('./studyPyQt/movie.png'))
 
         # 검색 버튼 클릭 시그널 / 슬롯 함수
         self.btnSearch.clicked.connect(self.btnSearchClicked)
@@ -45,19 +45,40 @@ class qtApp(QWidget):
         self.tblResult.setRowCount(len(items)) # 밑으로 몇개의 행을 만들어 줄것인지
         self.tblResult.setHorizontalHeaderLabels(['영화 제목', '개봉년도', '감독', '배우진', '평점', '링크', '포스터'])
         self.tblResult.setColumnWidth(0, 150)
-        self.tblResult.setColumnWidth(1, 60) # 개봉년도
+        self.tblResult.setColumnWidth(1, 70) # 개봉년도
         self.tblResult.setColumnWidth(4, 50) # 개봉년도
         self.tblResult.setEditTriggers(QAbstractItemView.NoEditTriggers) # 컬럼 데이터 수정 금지 못하도록
         
 
 
         for i, post in enumerate(items): # 0, 영화 타이틀
-            title = self.replaceHtmlTag(post['title'])
+            title = self.replaceHtmlTag(post['title']) # HTML 특수문자 변환 / 영어제목 가져오기 추가
+            subtitle = post['subtitle']
+            title = f'{title} ({subtitle})'
             pubDate = post['pubDate']
             director = post['director']
+            director = director.replace('|', ',')[:-1]
             actor = post['actor']
+            actor = actor.replace('|', ',')[:-1]
             userRating = post['userRating']
             link = post['link']
+            img_url = post['image']
+            # 23.03.08 => 포스터 이미지 추가
+            #print(img_url == '')
+            if img_url !='': # 빈값이면 포스터가 없음
+                data = urlopen(img_url).read() # 2진데이터 - 네이버 영화에 있는 이미지 다운, 텍스트형태의 데이터
+                image = QImage() # 이미지 객체
+                image.loadFromData(data)
+                # QTableWidget 이미지를 그냥 넣을 수 없음 => QLabel()에 집어넣은뒤 QLabel을 QTableWidget에 넣어줘야함
+                imgLabel = QLabel()
+                imgLabel.setPixmap(QPixmap(image))
+
+                #  data를 이미지로 저장가능!
+                # f = open(f'./studyPyQt/temp/image_{i+1}.png',mode='wb') #파일 쓰기
+                # f.write(data)
+                # f.close()
+
+
             #image = QImage(requests.get(post['image'], streagm=True))
             # imgData = urlopen(post['image']).read()
             # image = QPixmap()
@@ -77,8 +98,16 @@ class qtApp(QWidget):
             self.tblResult.setItem(i, 3, QTableWidgetItem(actor))
             self.tblResult.setItem(i, 4, QTableWidgetItem(userRating))
             self.tblResult.setItem(i, 5, QTableWidgetItem(link))
-            # if imgData != None:
-            #     self.tblResult.setItem(i, 6, imgLabel)
+            #self.tblResult.setItem(i, 6, QTableWidgetItem(img_url))
+            if img_url != '':
+                self.tblResult.setCellWidget(i, 6, imgLabel)
+                self.tblResult.setRowHeight(i, 110) # 포스터가 있으면 쉘 높이를 늘림
+            else:
+                self.tblResult.setItem(i, 6, QTableWidgetItem('No Poster!'))
+            
+
+
+            # self.tblResult.set
 
     def txtSearchReturned(self):
            self.btnSearchClicked()
