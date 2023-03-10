4d828b2c3647a6b69490ec35ba935b01fc982461
yeseoz <w12345678992@gmail.com>
2023-03-10 13:25:25 +0900

    수정 기능 추가

--- part1/studyPyQt/mp05_adressBookApp.py
+++ part1/studyPyQt/mp05_adressBookApp.py
@@ -7,28 +7,135 @@ import pymysql
 
 class qtApp(QMainWindow):
     conn = None
+    curIdx = 0 # 현재 데티어 PK
 
     def __init__(self):
         super().__init__()
         uic.loadUi('./studyPyQt/addressBook.ui',self)
         self.setWindowIcon(QIcon('./studyPyQt/addressBook.png'))
+        self.setWindowTitle('주소록 v0.5')
 
         self.initDB()
+
+        # 버튼 시그널 / 슬롯함수
+        self.btnNew.clicked.connect(self.btnNewClicked) 
+        self.btnSave.clicked.connect(self.btnSaveClicked) 
+        self.tblAddress.doubleClicked.connect(self.tblAddressDoubleClicked)
+        self.btnDel.cliced.connect(self.btnDelClicked)
+
+    def btnDelClicked(self):
+        pass
+
+    def btnNewClicked(self): # 신규버튼 누르면
+        # 라인 에디트 내용 삭제 후 이름에 포커스
+        self.txtName.setText('')
+        self.txtPhone.setText('')
+        self.txtEmail.setText('')
+        self.txtAddress.setText('')
+        self.txtName.setFocus()
+        self.curIdx = 0 # 0은 진짜 신규
+        print(self.curIdx)
+
+    def tblAddressDoubleClicked(self):
+        rowIndex = self.tblAddress.currentRow()
+        self.txtName.setText(self.tblAddress.item(rowIndex, 1).text())
+        self.txtPhone.setText(self.tblAddress.item(rowIndex, 2).text())
+        self.txtEmail.setText(self.tblAddress.item(rowIndex, 3).text())
+        self.txtAddress.setText(self.tblAddress.item(rowIndex, 4).text())
+        self.curIdx = int(self.tblAddress.item(rowIndex,0).text())
+        print(self.curIdx)
+
+    def btnSaveClicked(self): # 저장
+        fullName = self.txtName.text()
+        phoneNum = self.txtPhone.text()
+        email = self.txtEmail.text()
+        address = self.txtAddress.text()
+
+        # print(fullName,phoneNum,email,address)
+        # 이름과 전화번호를 입력하지 않으면 알람
+        if fullName == '' or phoneNum == '':
+            QMessageBox.warning(self,'주의', '이름과 핸드폰번호를 입력하세요!')
+            return # 진행불가
+        else:
+            self.conn = pymysql.connect(host='localhost', user= 'root', password='12345',
+                                    db='miniproject', charset='utf8' )
+            if self.curIdx == 0: # 신규
+            # 네개 변수값 받아서 INSERT 쿼리문 만들기
+                query = '''INSERT INTO addressbook (FullName,PhoneNum,Email,Address)
+				                 VALUE (%s, %s, %s, %s)'''
+            else:
+                query = '''UPDATE addressbook
+                              SET FullName = %s 
+                                , PhoneNum = %s
+                                , Email = %s
+                                , Address = %s
+                            WHERE Idx = %s''' # update 쿼리문
+
+            cur = self.conn.cursor()
+            if self.curIdx == 0:
+                cur.execute(query, (fullName, phoneNum, email, address))
+            else: 
+                cur.execute(query, (fullName, phoneNum, email, address, self.curIdx))
+
+            self.conn.commit()
+            self.conn.close()
+
+            # 저장 성공 메시지
+            if self.curIdx == 0:
+                QMessageBox.about(self, '성공', '저장 성공했습니다.')
+            else:
+                QMessageBox.about(self, '성공', '변경 성공했습니다.')
+            # QTableWidget 새 데이터가 출력되도록 
+            self.initDB()
+            # 입력창 내용 없어져
+            self.btnNewClicked()
+
 
     def initDB(self):
         self.conn = pymysql.connect(host='localhost', user= 'root', password='12345',
                                     db='miniproject', charset='utf8' )
         cur = self.conn.cursor()
-        query = 'SELECT * FROM addressbook'
+        query = '''SELECT Idx
+                        , FullName
+                        , PhoneNum
+                        , Email
+                        , Address
+                     FROM addressbook''' # 멀티라인 문자열 편함!
         cur.execute(query)
         rows = cur.fetchall()
 
-        print(rows)
+        #print(rows)
+        self.makeTable(rows)
         self.conn.close() # 프로그램 종료할 때
 
+    def makeTable(self, rows):
+        self.tblAddress.setSelectionMode(QAbstractItemView.SingleSelection) # 단일선택
+        self.tblAddress.setColumnCount(5)
+        self.tblAddress.setRowCount(len(rows))
+        self.tblAddress.setHorizontalHeaderLabels(['번호', '이름', '전화번호', '이메일', '주소']) # 1. 열제목
+        self.tblAddress.setColumnWidth(0, 0) # 번호는 숨김
+        self.tblAddress.setColumnWidth(1, 70) # 이름
+        self.tblAddress.setColumnWidth(2, 105) # 전화번호
+        self.tblAddress.setColumnWidth(3, 200) # 이메일
+        self.tblAddress.setColumnWidth(4, 200) # 주소
+        self.tblAddress.setEditTriggers(QAbstractItemView.NoEditTriggers) # 1. 컬럼 수정금지
+        
+        
+        for i, row in enumerate(rows):
+            # row[0] ~ row[4]
+            idx = row[0]
+            fullName = row[1]
+            phoneNum = row[2]
+            email = row[3]
+            address = row[4]
 
+            self.tblAddress.setItem(i, 0, QTableWidgetItem(str(idx)))
+            self.tblAddress.setItem(i, 1, QTableWidgetItem(fullName))
+            self.tblAddress.setItem(i, 2, QTableWidgetItem(phoneNum))
+            self.tblAddress.setItem(i, 3, QTableWidgetItem(email))
+            self.tblAddress.setItem(i, 4, QTableWidgetItem(address))
 
-
+        self.stbCurrent.showMessage(f'전체 주소록 : {len(rows)}개')
 
 
 
