379a80146df03d2d0cbe1fc480f14b59392cca84
yeseoz <w12345678992@gmail.com>
2023-05-08 08:53:36 +0900

    5월4일 못한거 커밋

---
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace SmartHomeMonitoringApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // <Frame> ==> Page.xaml
+            // <ContentControl> ==> UserControl.xaml 
+            ActiveItem.Content = new Views.DataBaseControl();
+        }
+
+        // 끝내기 버튼 클릭이벤트 핸들러
+        private void MnuExitProgram_Click(object sender, RoutedEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();  // 작업관리자에서 프로세스 종료!
+            Environment.Exit(0); // 둘중하나만 쓰면 됨
+        }
+    }
+}
